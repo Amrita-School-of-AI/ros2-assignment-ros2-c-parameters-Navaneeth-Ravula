1026fb8e5a9c293ac6de9d86c9c70867943cf759
Navaneeth-Ravula <ravulanavaneeth@gmail.com>
2026-02-20 10:01:23 +0530

    Done

--- src/param_node.cpp
+++ src/param_node.cpp
@@ -26,13 +26,35 @@ public:
     ParamNode()
         : Node("param_node")
     {
-        // TODO: Declare parameters here
+        // Declare parameters here
+        this->declare_parameter<std::string>("robot_name", "ROS2Bot");
+        this->declare_parameter<double>("max_speed", 1.5);
+        this->declare_parameter<bool>("enabled", true);
 
-        // TODO: Create timer here
+        // Create timer here
+        timer_ = this->create_wall_timer(
+            2000ms,
+             std::bind(&ParamNode::timer_callback, this)
+        );
     }
 
 private:
     // TODO: Define timer_callback function here
+    // Define timer_callback function here
+void timer_callback()
+{
+    std::string robot_name = this->get_parameter("robot_name").as_string();
+    double max_speed = this->get_parameter("max_speed").as_double();
+    bool enabled = this->get_parameter("enabled").as_bool();
+
+    RCLCPP_INFO(
+        this->get_logger(),
+        "Robot: %s, Max Speed: %f, Enabled: %s",
+        robot_name.c_str(),
+        max_speed,
+        enabled ? "true" : "false"
+    );
+}
 
     rclcpp::TimerBase::SharedPtr timer_;
 };
